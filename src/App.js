74eda606361f3e07755f7270076d4a07fe64cf50
Ganesh babu M <kbtganesh@gmail.com>
2019-01-10 11:09:00 +0530

    react version upgraded to 17-alpha

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,22 +8,42 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hooks
           </p>
-          <a
+          {/* <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
+          </a> */}
+          <Example />
         </header>
       </div>
     );
   }
+}
+
+const Example = () => {
+  // Declare a new state variable, which we'll call "count"
+  const [Count, setCountkbt] = useState(0);
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${Count} times, updated directly to DOM`;
+  });
+  // let count = 1;
+
+  return (
+    <div>
+      <p>You Clicked {Count} timeskbt</p>
+      <button onClick={()=>setCountkbt(Count+1)}>
+        Click me
+      </button>
+    </div>
+  );
 }
 
 export default App;
